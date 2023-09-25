0aa902d93413209033e5f8ff899ba1911f444c51
duhanin <duhanin.av0404@gmail.com>
2023-09-25 18:50:41 +0500

    second

--- practic/25_09.py
+++ practic/25_09.py
@@ -7,5 +7,23 @@ def first():
 	print("Перевёрнутый:")
 	print(l[::-1])
 
+def second():
+	l1 = [randint(0, 100) for x in range(10)]
+	l2 = [randint(0, 100) for x in range(10)]
+	
+	print("Первый список:")
+	print(l1)
+	print("Второй список:")
+	print(l2)
+
+	l3 = list()
+	l3 += l1[::2]
+	l3 += l2[1::2]
+	
+	print("Третий список")
+	print(l3)
+
+
 if __name__ == '__main__':
-	first()
+	#first()
+	second()
